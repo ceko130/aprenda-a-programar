2bb7ac2fbf29682f94f0d72a7b7d968bc0ef2b92
Cesar Kouichi <cekogl13@gmail.com>
2022-08-25 22:56:18 -0300

    pratica-1-js-desafio

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,7 +1,9 @@
-Nome = "Cesar"
-Email = "cekogl13@gmail.com"
-Profissao = "Comerciante"
-Idade = 19
-lugar = "canada"
+let Nome = "Cesar"
+let Email = "cekogl13@gmail.com"
+let Profissao = "Comerciante"
+let Idade = 19
+let lugar = "canada"
 
 
+
+console.log(`Ola,${Nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugar}. Confirma pra gente alguns dados? A sua idade é ${Idade}, você é ${Profissao} e o seu email é ${Email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
